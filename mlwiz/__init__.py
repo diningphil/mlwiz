7ac1a17da6eb6b42b558a446658e2799ef451c08
Federico Errica <federico.errica@protonmail.com>
2025-12-02 18:18:00 +0000

    added static and util

--- mlwiz/__init__.py
+++ mlwiz/__init__.py
@@ -1,3 +1,12 @@
-from . import data, evaluation, experiment, log, model, training
+from . import data, evaluation, experiment, log, model, static, training, util
 
-__all__ = ["data", "evaluation", "experiment", "log", "model", "training"]
+__all__ = [
+    "data",
+    "evaluation",
+    "experiment",
+    "log",
+    "model",
+    "static",
+    "training",
+    "util",
+]
