503e7e9b185c7d79ff8a2f202d2f4ae0722c3bac
Federico Errica <f.errica@protonmail.com>
2026-02-12 17:02:22 +0100

    Update release version to 1.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = "2024-present, Federico Errica"
 author = "Federico Errica"
 
 # The full version, including alpha/beta/rc tags
-release = "1.4.0"
+release = "1.4.1"
 
 
 # -- General configuration ---------------------------------------------------
